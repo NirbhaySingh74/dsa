9f36ce0ca1078026584b00d2b38338ee8ce493ae
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2024-11-17 00:25:15 +0530

    pair

---
+++ basics/input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout << "Enter Your Name" << endl;
+    cin >> name;
+    cout << "Hello " << name << ", How are you?" << endl;
+    return 0;
+}
