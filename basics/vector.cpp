976373cc9e9742a674607fcd658b650c178eff37
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2024-11-17 11:21:17 +0530

    other operations on vector

--- basics/vector.cpp
+++ basics/vector.cpp
@@ -11,6 +11,8 @@ int main()
     v.push_back(20);
     v.push_back(30);
     v.push_back(40);
+    v.push_back(50);
+    v.push_back(60);
 
     // Display Element
     for (int i = 0; i < v.size(); i++)
@@ -25,6 +27,27 @@ int main()
     for (int i = 0; i < v.size(); i++)
     {
         cout << v[i] << " ";
+    }
+    cout << endl;
+    // iterate on vector using auto
+    for (auto it = v.begin(); it != v.end(); it++)
+    {
+        cout << *(it) << " ";
+    }
+    cout << endl;
+    // iterate using foreach loop
+    for (auto it : v)
+    {
+        cout << it << " ";
+    }
+
+    //    remove element using erase property
+    v.erase(v.begin()+1);
+    cout << endl
+         << "after removing second element" << endl;
+    for (auto it : v)
+    {
+        cout << it << " ";
     }
     return 0;
 }
