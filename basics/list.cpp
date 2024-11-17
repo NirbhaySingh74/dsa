b7719bac1b6b6309a4d83e49db1fc522e49280fe
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2024-11-17 11:29:22 +0530

    list

---
+++ basics/list.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <list>
+using namespace std;
+int main()
+{
+    list<int> ls;
+    ls.push_back(2);
+    ls.push_back(4);
+    ls.push_front(1);
+    for (auto it : ls)
+    {
+        cout << it << " ";
+    }
+    // rest functions same as vector
+    // begin, end, rbegin, rend, clear, insert, size, swap
+}
