fc291a29af2d977b7f682956b3647b1dc7a11449
Nirbhay singh <105772373+NirbhaySingh74@users.noreply.github.com>
2024-12-06 10:34:04 +0530

    palindrome number

--- basicmaths/revNum.cpp
+++ basicmaths/revNum.cpp
@@ -12,10 +12,32 @@ int revnum(int n)
     }
     return revNum;
 }
+
+void checkPalindrome(int n)
+{
+    int revNum = 0, rem = 1, originalNum = n;
+    while (n > 0)
+    {
+        rem = n % 10;
+        revNum = (revNum * 10) + rem;
+        n = n / 10;
+    }
+
+    if (originalNum == revNum)
+    {
+        cout << "Number is palindrome" << endl;
+    }
+    else
+    {
+        cout << "Number is not palindrome" << endl;
+    }
+}
+
 int main()
 {
     int n;
-    cout << "Enter The Number You want to reverse" << endl;
+    cout << "Enter The Number" << endl;
     cin >> n;
     cout << revnum(n) << endl;
+    checkPalindrome(n);
 }
