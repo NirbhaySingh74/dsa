c61abf8a229cb39991ca87cc8dfc952c186c1b63
Nirbhay singh <105772373+NirbhaySingh74@users.noreply.github.com>
2024-12-06 20:54:37 +0530

    1 to n

--- recursion/basics.cpp
+++ recursion/basics.cpp
@@ -5,13 +5,25 @@ void printNTo1(int n)
 {
     if (n == 0)
     {
+        cout << endl;
         return;
     }
     cout << n << " ";
     printNTo1(n - 1);
 }
 
+void print1toN(int current, int n)
+{
+    if (current > n)
+    {
+        return;
+    }
+    cout << current << " ";
+    print1toN(current + 1, n);
+}
+
 int main()
 {
     printNTo1(10);
+    print1toN(1, 10);
 }
