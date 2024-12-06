e8d102b24607b453bf8590255ee00417fdb55eae
Nirbhay singh <105772373+NirbhaySingh74@users.noreply.github.com>
2024-12-06 19:46:27 +0530

    armstrong number

--- basicmaths/revNum.cpp
+++ basicmaths/revNum.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int revnum(int n)
@@ -33,11 +34,41 @@ void checkPalindrome(int n)
     }
 }
 
+bool isArmstrong(int num)
+{
+
+    int k = to_string(num).length();
+
+    int sum = 0;
+
+    int n = num;
+
+    while (n > 0)
+    {
+
+        int ld = n % 10;
+
+        sum += pow(ld, k);
+
+        n = n / 10;
+    }
+
+    return sum == num;
+}
+
 int main()
 {
     int n;
     cout << "Enter The Number" << endl;
     cin >> n;
-    cout << revnum(n) << endl;
-    checkPalindrome(n);
+    // cout << revnum(n) << endl;
+    // checkPalindrome(n);
+    if (isArmstrong(n))
+    {
+        cout << n << " is an Armstrong number." << endl;
+    }
+    else
+    {
+        cout << n << " is not an Armstrong number." << endl;
+    }
 }
