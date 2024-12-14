8290e27b038d07b3217310c5b4082cdc9d572d1b
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2024-12-14 15:41:16 +0530

    secondlargest optimal approach

--- array/easy/largest.cpp
+++ array/easy/largest.cpp
@@ -8,19 +8,60 @@ int largest_Element(vector<int> &arr)
     return *max_element(arr.begin(), arr.end());
 }
 
-int largest_ElementInArr(vector<int> &arr) {
+int largest_ElementInArr(vector<int> &arr)
+{
     int largest = arr[0];
-    for(int i=0;i<arr.size();i++) {
-        if(arr[i] > largest) {
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] > largest)
+        {
             largest = arr[i];
         }
     }
     return largest;
 }
 
+int second_largest(vector<int> &arr)
+{
+    int largest = arr[0], secondLargest = -1;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] > secondLargest && arr[i] < largest)
+        {
+            secondLargest = arr[i];
+        }
+    }
+
+    return secondLargest;
+}
+
+int secondLargestOptimal(vector<int> &arr)
+{
+    int largest = 1, secondargest = -1;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] > largest)
+        {
+            secondargest = largest;
+            largest = arr[i];
+        }
+    }
+    return secondargest;
+}
+
 int main()
 {
     vector<int> arr = {12, 4, 5, 19, 34, 6, 9};
     // cout << largest_Element(arr);
-    cout<<largest_ElementInArr(arr);
+    // cout << largest_ElementInArr(arr) << " ";
+    // cout << second_largest(arr);
+    cout<<secondLargestOptimal(arr);
 }
