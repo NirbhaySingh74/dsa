e989b5c546c8a7afabcbb850b4d5d673720bc7e1
Nirbhay singh <105772373+NirbhaySingh74@users.noreply.github.com>
2024-12-21 22:25:55 +0530

    rotate array

--- array/easy/rotation.cpp
+++ array/easy/rotation.cpp
@@ -41,13 +41,25 @@ void left_Rotate_By_K_Optimal(int arr[], int n, int k)
     reverse(arr, arr + n);
 }
 
+void right_rotate_1_place(int arr[], int n)
+{
+    int temp = arr[n - 1];
+    // arr[0] = temp;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = temp;
+}
+
 int main()
 {
     vector<int> arr = {1, 2, 3, 4, 5, 6};
-    int nums[] = {1, 2, 3, 4, 5, 6};
+    int nums[] = {1, 2, 3, 4, 5, 6, 7};
     // leftRoteteBy1Place(arr, arr.size());
     // left_Rotate_By_K_Place(arr, 7);
-    left_Rotate_By_K_Optimal(nums, 6, 4);
+    // left_Rotate_By_K_Optimal(nums, 6, 4);
+    right_rotate_1_place(nums, 7);
     for (int num : nums)
     {
         cout << num << " ";
