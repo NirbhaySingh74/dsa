8d88a1ca2e94b8d7ec9eeead52920bc4dc8eb48b
Nirbhay singh <105772373+NirbhaySingh74@users.noreply.github.com>
2024-12-26 09:57:51 +0530

    find single num in array

--- array/easy/intersection.cpp
+++ array/easy/intersection.cpp
@@ -2,27 +2,6 @@
 #include <iostream>
 using namespace std;
 
-void find_union(vector<int> &arr1, vector<int> &arr2)
-{
-    set<int> st;
-    for (int num : arr1)
-    {
-
-        st.insert(num); // Insert num
-    }
-
-    for (int num : arr2)
-    {
-
-        st.insert(num); // Insert num
-    }
-
-    for (auto it : st)
-    {
-        cout << it << " ";
-    }
-}
-
 void find_Intersection(vector<int> &a, vector<int> &b)
 {
     int n1 = a.size(), n2 = b.size();
@@ -48,8 +27,10 @@ void find_Intersection(vector<int> &a, vector<int> &b)
 
 int main()
 {
-    vector<int> arr1 = {1, 2, 2, 3, 3, 4, 5, 6};
-    vector<int> arr2 = {2, 3, 3, 5, 6, 6, 7};
-    // find_union(arr1, arr2);
-    find_Intersection(arr1, arr2);
+    vector<int> a = {1, 2, 2, 3, 4, 4, 5, 6};
+    vector<int> b = {2, 3, 3, 5, 6, 6, 7};
+    find_Intersection(a, b);
+    cout << "hello";
+
+    return 0;
 }
