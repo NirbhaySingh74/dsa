4c496737e5a494f6fe1f2305deac96b8d3e0491c
Nirbhay singh <105772373+NirbhaySingh74@users.noreply.github.com>
2024-12-28 08:53:53 +0530

    Longest subarray with 2 loop

--- array/easy/longestsub.cpp
+++ array/easy/longestsub.cpp
@@ -23,9 +23,30 @@ int Longest_Subarray_with_sum_K(vector<int> &nums, int target)
     return len;
 }
 
+int Longest_Subarray_with_sum_2Loop(vector<int> &nums, int target)
+{
+    int len = 0, n = nums.size(), j;
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (j = i; j < n; j++)
+        {
+            sum = sum + nums[j];
+            if (sum == target)
+            {
+                len = max(len, j - i + 1);
+            }
+            cout << nums[j] << " ";
+        }
+        cout << endl;
+    }
+    return len;
+}
+
 int main()
 {
-    vector<int> nums = {1, 2, 4, 3, 2, 1, 1, 4, 5};
-    cout << Longest_Subarray_with_sum_K(nums, 3) << endl;
+    vector<int> nums = {1, 2, 3, 4, 5};
+    // cout << Longest_Subarray_with_sum_K(nums, 3) << endl;
+    cout << Longest_Subarray_with_sum_2Loop(nums, 3) << endl;
     return 0;
 }
