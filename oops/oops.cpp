8861f5ebb1451b7188624eaefd5745af0d89bb2b
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-01-12 19:46:51 +0530

    oops attribute

---
+++ oops/oops.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class Teacher
+{
+    // attributes, properties
+    string name;
+    string dept;
+    string dept;
+    string subject;
+    double salary;
+};
+
+int main()
+{
+    cout << "oops";
+}
