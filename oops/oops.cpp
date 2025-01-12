ac4c862983ac73786b58d255641a90095ac8a4ad
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-01-12 20:02:08 +0530

    methods in oops

--- oops/oops.cpp
+++ oops/oops.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 class Teacher
 {
-    // attributes, properties
+    // attributes/ properties
     string name;
     string dept;
     string dept;
     string subject;
     double salary;
+
+    // methods / member Function
+    void changeDept(string newDept)
+    {
+        dept = newDept;
+    }
 };
 
 int main()
