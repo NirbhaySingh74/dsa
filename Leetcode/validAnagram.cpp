e5fb2e7cea321db0984247822d2df2b7263e5f56
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-01-18 09:42:19 +0530

    Longest subarray sum

--- Leetcode/validAnagram.cpp
+++ Leetcode/validAnagram.cpp
@@ -2,9 +2,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+void bruteApproach(string s1, string s2)
 {
-    string s1 = "nagaam", s2 = "anagram";
+    // Time complexity will be 0(m logm) for sort s1 and o(n log n) for sort s2 thus total time complexity will be -> O(mlogm+nlogn)
     sort(s1.begin(), s1.end());
     sort(s2.begin(), s2.end());
     if (s1 == s2)
@@ -15,5 +15,18 @@ int main()
     {
         cout << "False";
     }
+}
+
+void betterApproach(string s1, string s2)
+{
+    stack<string> st;
+    st.push(s1);
+    st.push(s2);
+}
+
+int main()
+{
+    string s1 = "nagaam", s2 = "anagram";
+    bruteApproach(s1, s2);
     return 0;
 }
