5424650843eb80189afcce768591993990839bab
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-01-19 09:22:00 +0530

    Second Largest Element in Array

--- Leetcode/easy/Larget.cpp
+++ Leetcode/easy/Larget.cpp
@@ -2,6 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// This is optimal approach to find Largest in Array we can also solve it by using sorting the array but that will take O(N*log(N)) and by using this algorithm is is taking 0(n) Time complexity and space complexity is 0(1)
 int FindMax(vector<int> &nums)
 {
     int num = nums[0];
@@ -15,8 +16,29 @@ int FindMax(vector<int> &nums)
     return num;
 }
 
+// second Larget in Array
+int secondLargest(vector<int> &nums)
+{
+    int largest = nums[0], slargest = -1;
+
+    for (int num : nums)
+    {
+        if (num > largest)
+        {
+            slargest = largest;
+            largest = num;
+        }
+        else if (num < largest && num > slargest)
+        {
+            slargest = num;
+        }
+    }
+
+    return slargest;
+}
 int main()
 {
-    vector<int> nums = {2, 3, 4, 1, 3, 14, 3, 5};
-    cout << FindMax(nums);
+    vector<int> nums = {1, 2, 4, 7, 7, 5};
+    cout << "Largest : " << FindMax(nums) << endl;
+    cout << "Second Largest : " << secondLargest(nums);
 }
