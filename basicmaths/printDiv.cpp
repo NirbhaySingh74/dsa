47dd6adb611ad835294285035fa9326b4831e0ad
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-02-02 15:29:20 +0530

    optimal approach to print divisible

--- basicmaths/printDiv.cpp
+++ basicmaths/printDiv.cpp
@@ -13,8 +13,25 @@ void printAllDiv(int num)
     }
 }
 
+// optimal approach to print division
+void optimalPrintDiv(int num)
+{
+    for (int i = 1; i <= sqrt(num); i++)
+    {
+        if (num % i == 0)
+        {
+            cout << i << " ";
+            if ((num / i) != i)
+            {
+                cout << num / i << " ";
+            }
+        }
+    }
+}
+
 int main()
 {
     int num = 36;
-    printAllDiv(num);
+    // printAllDiv(num);
+    optimalPrintDiv(num);
 }
