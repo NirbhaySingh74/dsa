0d8e0589004b5d13476b2ca38af58da5ca0f60b0
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-02-03 10:38:18 +0530

    Print 1 To N Linear

--- recursion/L2.cpp
+++ recursion/L2.cpp
@@ -9,7 +9,21 @@ void printName(int count)
     cout << "Nirbhay" << endl;
     printName(count + 1);
 }
+
+// Print Linerally from 1 To N
+void print1ToNLin(int n, int i)
+{
+    if (i > n)
+        return;
+    cout << i << " ";
+    print1ToNLin(n, i + 1);
+}
 int main()
 {
-    printName(1);
+    int n;
+    cout << "Enter N" << endl;
+    cin >> n;
+    // printName(1);
+    print1ToNLin(n, 1);
+    return 0;
 }
