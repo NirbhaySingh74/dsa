e4e1ca2d52d5163227d9bde55d8336ef9b7bd0f6
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-02-03 08:04:44 +0530

    GCD Optimized Code

--- basicmaths/gcd.cpp
+++ basicmaths/gcd.cpp
@@ -2,21 +2,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// this takes o(N) Time complexity which is not efficient
 int gcd(int num1, int num2)
 {
-    int ans = 0;
-    for (int i = 1; i < min(num1, num2); i++)
+
+    for (int i = min(num1, num2); i >= 1; i--)
     {
         if (num1 % i == 0 && num2 % i == 0)
         {
-            ans = max(ans, i);
+
+            return i;
+            break;
         }
     }
-    return ans;
+    return -1;
+}
+// Euclidean Algorithm This approach runs in O(log N) time
+int gcd2(int num1, int num2)
+{
+    while (num2 != 0)
+    {
+        int temp = num2;
+        num2 = num1 % num2;
+        // cout << "num2 - " << num2 << endl;
+        num1 = temp;
+        // cout << "num1 - " << num1 << endl;
+    }
+    return num1;
 }
 
 int main()
 {
     int firstNum = 12, secondNum = 18;
-    cout << gcd(firstNum, secondNum);
+    cout << gcd2(firstNum, secondNum);
 }
