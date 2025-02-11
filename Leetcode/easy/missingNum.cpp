3947514841854fe0b98a4f25fed327ca75e3fea0
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-02-11 09:43:48 +0530

    better approach

--- Leetcode/easy/missingNum.cpp
+++ Leetcode/easy/missingNum.cpp
@@ -23,8 +23,29 @@ int bruteMissingNum(vector<int> &nums)
     return -1;
 }
 
+// better approach which takes o (n) time complexity and o(n) space complexity
+int betteorMissingNum(vector<int> &nums)
+{
+    vector<int> temp(nums.size() + 1, 0);
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        temp[i] = 1;
+    }
+
+    for (int i = 1; i <= temp.size(); i++)
+    {
+        if (temp[i] == 0)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
-    vector<int> nums = {1, 2, 3, 5};
-    cout << bruteMissingNum(nums);
+    vector<int> nums = {1, 2, 3, 4, 6};
+    // cout << bruteMissingNum(nums);
+    cout << betteorMissingNum(nums);
 }
