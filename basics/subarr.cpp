d593627cb01c5819c73dad1a9acafbe87597fdf6
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-02-13 16:17:03 +0530

    using 2 loops

--- basics/subarr.cpp
+++ basics/subarr.cpp
@@ -25,8 +25,29 @@ int longest_subarr(vector<int> &nums, int k)
     return mx;
 }
 
+// using 2 loop it takes o(n^2) Time complexity
+int longest_subarrtwoloop(vector<int> &nums, int k)
+{
+    int mx = 0, n = nums.size();
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            sum = sum + nums[j];
+            if (sum == k)
+            {
+
+                mx = max(mx, (j - i) + 1);
+            }
+        }
+    }
+    return mx;
+}
+
 int main()
 {
     vector<int> nums = {2, 3, 2, 3, 1, 9};
-    cout << longest_subarr(nums, 13) << endl;
+    // cout << longest_subarr(nums, 13) << endl;
+    cout << longest_subarrtwoloop(nums, 7) << endl;
 }
