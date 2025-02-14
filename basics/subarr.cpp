24793e9e2108914c9ba01186c7633817ffcc355b
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-02-14 07:38:03 +0530

    subarr optimal one

--- basics/subarr.cpp
+++ basics/subarr.cpp
@@ -45,9 +45,32 @@ int longest_subarrtwoloop(vector<int> &nums, int k)
     return mx;
 }
 
+// optimal approach 2 pointer which takes o(2 ^ n) time complexity and o(1) space complexity
+int optimal_twopointer(vector<int> &nums, int k)
+{
+    int left = 0, right = 0, sum = nums[right], maxLen = 0;
+    while (right < nums.size())
+    {
+
+        while (left <= right && sum > k)
+        {
+            sum = sum - nums[left];
+            left++;
+        }
+        if (sum == k)
+        {
+            maxLen = max(maxLen, right - left + 1);
+        }
+        right++;
+        sum += nums[right];
+    }
+    return maxLen;
+}
+
 int main()
 {
-    vector<int> nums = {2, 3, 2, 3, 1, 9};
+    vector<int> nums = {2, 3, 2, 3, 2, 9};
     // cout << longest_subarr(nums, 13) << endl;
-    cout << longest_subarrtwoloop(nums, 7) << endl;
+    // cout << longest_subarrtwoloop(nums, 7) << endl;
+    cout << optimal_twopointer(nums, 9) << endl;
 }
