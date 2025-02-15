669f7652a285dee1a863c5aee9021e9b303329c2
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-02-15 10:14:31 +0530

    using hashmap majority element

--- Leetcode/medium/majority.cpp
+++ Leetcode/medium/majority.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 // bruteforce taking near about o(n^2) Time complexity
@@ -9,13 +9,16 @@ int brute_findMajorityNum(vector<int> &nums, int N)
     for (int i = 0; i < N - 1; i++)
     {
         count = 1;
+        int howmanytimes = 0;
         for (int j = i + 1; j < N; j++)
         {
             if (nums[i] == nums[j])
             {
                 count++;
             }
+            howmanytimes++;
         }
+        cout << "For : " << nums[i] << " inner Loop runs : " << howmanytimes << "times" << endl;
         // cout << count << " " << endl;
         if (count > N / 2)
             return nums[i];
@@ -23,9 +26,27 @@ int brute_findMajorityNum(vector<int> &nums, int N)
     return -1;
 }
 
+// better approach using map which takes o(N log N) time complexity
+int better_approach(vector<int> &nums, int N)
+{
+    map<int, int> mp;
+    for (int i = 0; i < N; i++)
+    {
+        mp[nums[i]]++;
+    }
+
+    for (auto it : mp)
+    {
+        if (it.second > (N / 2))
+            return it.first;
+    }
+    return -1;
+}
+
 int main()
 {
-    vector<int> nums = {3, 2, 3, 4, 3, 1, 3};
-    cout << brute_findMajorityNum(nums, nums.size());
+    vector<int> nums = {3, 2, 3, 4, 4, 4, 4};
+    // cout << brute_findMajorityNum(nums, nums.size());
+    cout << better_approach(nums, nums.size());
     return 0;
 }
