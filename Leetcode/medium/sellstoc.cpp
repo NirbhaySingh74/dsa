bbf3741557b13d39aa4c1b0a6d5b2d84a06bdc89
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-02-16 05:51:17 +0530

    Buy ans sell stocks optimal

--- Leetcode/medium/sellstoc.cpp
+++ Leetcode/medium/sellstoc.cpp
@@ -16,9 +16,23 @@ int brute_buy_sell_stock(vector<int> &prices)
     }
     return maxProfit;
 }
+
+// optimal appraoch taking o(n) time
+int optimal_BuyAndSell_Stocks(vector<int> &prices)
+{
+    int maxProfit = 0, mini = prices[0];
+    for (int i = 1; i < prices.size(); i++)
+    {
+        int cost = prices[i] - mini;
+        maxProfit = max(maxProfit, cost);
+        mini = min(mini, prices[i]);
+    }
+    return maxProfit;
+}
 int main()
 {
-    vector<int> prices = {7, 6, 4, 3, 1};
-    cout << brute_buy_sell_stock(prices);
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+    // cout << brute_buy_sell_stock(prices);
+    cout << optimal_BuyAndSell_Stocks(prices);
     return 0;
 }
