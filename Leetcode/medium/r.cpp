b7a0455de26e6a3451e3314dfd526c0de2b8723f
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-02-18 08:23:59 +0530

    remove duplicate 2 return index

--- Leetcode/medium/r.cpp
+++ Leetcode/medium/r.cpp
@@ -33,6 +33,36 @@ void removeDuplicate2(vector<int> &nums)
     nums.resize(j);
 }
 
+int removeDuplicates(vector<int> &nums)
+{
+    int n = nums.size();
+    if (n <= 2)
+        return n;
+
+    int j = 1;
+    int count = 1;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i] == nums[i - 1])
+        {
+            count++;
+        }
+        else
+        {
+            count = 1;
+        }
+
+        if (count <= 2)
+        {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+
+    return j;
+}
+
 int main()
 {
     vector<int> nums = {1, 1, 1, 2, 2, 3};
