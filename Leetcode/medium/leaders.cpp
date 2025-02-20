24e2fc63ad0dca46698236237cc7449d067478cf
Nirbhay Singh <nirbhaysingh943012@gmail.com>
2025-02-20 07:06:19 +0530

    Leaders in An array optimal

--- Leetcode/medium/leaders.cpp
+++ Leetcode/medium/leaders.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
+// brute force approach takes o(n^2) Time complexity
 vector<int> printLeader_IN_Arr(vector<int> &nums, int n)
 {
     vector<int> ans;
@@ -23,11 +24,30 @@ vector<int> printLeader_IN_Arr(vector<int> &nums, int n)
     return ans;
 }
 
+// optimal approach from back side takes o(n) time
+
+vector<int> optimal_Leaders(vector<int> &nums)
+{
+    int n = nums.size(), maxi = INT8_MIN;
+    vector<int> ans;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (nums[i] > maxi)
+        {
+            maxi = nums[i];
+            ans.push_back(nums[i]);
+        }
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+
 int main()
 {
     vector<int> nums = {10, 22, 12, 0, 4, 8};
     vector<int> ans;
-    ans = printLeader_IN_Arr(nums, nums.size());
+    // ans = printLeader_IN_Arr(nums, nums.size());
+    ans = optimal_Leaders(nums);
     for (int num : ans)
     {
         cout << num << " ";
